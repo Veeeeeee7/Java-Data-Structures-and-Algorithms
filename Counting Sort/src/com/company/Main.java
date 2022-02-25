b5bc3adeaabb3dfa37cb5f10e7215b068a4a35c8
Veee <victorli1076@gmail.com>
2022-02-25 08:56:28 -0800

    started counting sort

---
+++ Counting Sort/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] intArray = {2, 5, 9, 8, 2, 8 , 7, 10, 4, 3};
+
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+}
