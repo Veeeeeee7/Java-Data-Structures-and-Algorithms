74dec673b810deebea58381eb5131b664a8a9c10
victorli999 <victorli1076@gmail.com>
2022-10-03 21:00:15 -0700

    Recursion

--- Sorts/Recursion.java
+++ Sorts/Recursion.java
@@ -4,14 +4,17 @@ public class Recursion {
         System.out.println(iterativeFactorial(5));
     }
 
+    // recursive factorial
+    // each time going through, the number is multiplied by the number - 1 until the number is 0
     public static int recursiveFactorial(int num) {
         if (num == 0) {
             return 1;
         }
-
         return num * recursiveFactorial(num-1);
     }
 
+    // iterative factorial
+    // uses a for loop to multiply the number by the number - 1 until the number is 0
     public static int iterativeFactorial(int num) {
         if (num == 0) {
             return 1;
