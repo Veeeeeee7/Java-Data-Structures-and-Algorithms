06613069d5ac8b80cabb6f9a92ecdc800aab2518
victorli999 <victorli1076@gmail.com>
2022-11-22 14:33:20 -0800

    Circular Queue

--- Queues/CircularImplementation/CircularQueue.java
+++ Queues/CircularImplementation/CircularQueue.java
@@ -1,5 +1,7 @@
 package Queues.CircularImplementation;
 
+import java.util.NoSuchElementException;
+
 public class CircularQueue {
     private Employee[] queue;
     private int front;
@@ -12,21 +14,31 @@ public class CircularQueue {
 
     // Adds an item to the back of the queue
     public void add(Employee employee) {
-        if (back == queue.length) {
+        if (size() == queue.length - 1) {
+            int numItems = size();
+
             // Need to resize the backing array
             Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue, 0, newArray, 0, queue.length);
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue, 0, newArray, queue.length - front, back);
             queue = newArray;
+
+            front = 0;
+            back = numItems;
         }
 
         queue[back] = employee;
-        back++;
+        if (back < queue.length - 1) {
+            back++;
+        } else {
+            back = 0;
+        }
     }
 
     // Removes an item from the front of the queue
     public Employee remove() {
         if (size() == 0) {
-            return null;
+            throw new NoSuchElementException();
         }
 
         Employee employee = queue[front];
@@ -36,6 +48,8 @@ public class CircularQueue {
         if (size() == 0) {
             front = 0;
             back = 0;
+        } else if (front == queue.length) {
+            front = 0;
         }
 
         return employee;
@@ -44,7 +58,7 @@ public class CircularQueue {
     // Returns the item at the front of the queue
     public Employee peek() {
         if (size() == 0) {
-            return null;
+            throw new NoSuchElementException();
         }
 
         return queue[front];
@@ -52,13 +66,26 @@ public class CircularQueue {
 
     // Returns the size of the queue
     public int size() {
-        return back - front;
+        if (front <= back) {
+            return back - front;
+        } else {
+            return back - front + queue.length;
+        }
     }
 
     // Prints the queue
     public void printQueue() {
-        for (int i = front; i < back; i++) {
-            System.out.println(queue[i]);
+        if (front <= back) {
+            for (int i = front; i < back; i++) {
+                System.out.println(queue[i]);
+            }
+        } else {
+            for (int i = front; i < queue.length; i++) {
+                System.out.println(queue[i]);
+            }
+            for (int i = 0; i < back; i++) {
+                System.out.println(queue[i]);
+            }
         }
     }
 }
