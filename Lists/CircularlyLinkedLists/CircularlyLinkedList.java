712e01b7cb0114c60d2c3b2867237ebd6033e55e
victorli999 <victorli1076@gmail.com>
2022-12-30 16:17:38 -0800

    Added Remove

--- Lists/CircularlyLinkedLists/CircularlyLinkedList.java
+++ Lists/CircularlyLinkedLists/CircularlyLinkedList.java
@@ -4,8 +4,8 @@ public class CircularlyLinkedList {
     private Node head = null;
     private Node tail = null;
 
-    // Adds a node to the end of the list (ie between the previous tail and head)
-    public void addNode(int data) {
+    // Adds a node to the front of the list (ie between the previous tail and head)
+    public void add(int data) {
         Node newNode = new Node(data);
 
         // If the list is empty, set the head and tail to the new node
@@ -14,11 +14,31 @@ public class CircularlyLinkedList {
             tail = newNode;
             newNode.next = head;
         }
-        // Otherwise, set the node after tail to the new node, change the tail to the
-        // new node, and set the head to be the node after the new node
+        // Otherwise, save the current head to temp, set the new node as the head, set
+        // the node after head to temp, and set the node after tail to the new node
         else {
-            tail.next = newNode;
-            tail = newNode;
+            Node temp = head;
+            head = newNode;
+            head.next = temp;
+            tail.next = head;
+        }
+    }
+
+    // Removes a node from the front of the list
+    public void remove() {
+        // If the list is empty, print "List is empty"
+        if (head == null) {
+            System.out.println("List is empty");
+        }
+        // If the list has only one node, set the head and tail to null
+        else if (head == tail) {
+            head = null;
+            tail = null;
+        }
+        // Otherwise, set the head to the node after the head, and set the node after
+        // the tail to the new head
+        else {
+            head = head.next;
             tail.next = head;
         }
     }
--- Lists/CircularlyLinkedLists/Main.java
+++ Lists/CircularlyLinkedLists/Main.java
@@ -3,11 +3,15 @@ package Lists.CircularlyLinkedLists;
 public class Main {
     public static void main(String[] args) {
         CircularlyLinkedList cll = new CircularlyLinkedList();
-        cll.addNode(5);
-        cll.addNode(8);
-        cll.addNode(-2);
-        cll.addNode(11);
+        cll.add(5);
+        cll.add(8);
+        cll.add(-2);
+        cll.add(11);
 
+        cll.printList();
+
+        cll.remove();
+        System.out.println();
         cll.printList();
     }
 }
