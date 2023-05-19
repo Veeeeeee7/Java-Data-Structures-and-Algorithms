3765bdc2d8f00a14ea25c541e9ac10974c008edb
Veee <victorli1076@gmail.com>
2023-05-19 09:04:23 -0700

    Two Pointer to Add

---
+++ Algorithms/TwoPointer.java
@@ -0,0 +1,46 @@
+package Algorithms;
+
+import java.util.*;
+
+public class TwoPointer {
+    public static void main(String[] args) {
+        TwoPointer tp = new TwoPointer();
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
+        List<List<Integer>> res = tp.threeSum(nums);
+        System.out.println(res);
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length && nums[i] <= 0; i++) {
+            if (i == 0 || nums[i] != nums[i - 1]) {
+                twoSum(nums, i, res);
+            }
+        }
+        return res;
+    }
+
+    public void twoSum(int[] nums, int i, List<List<Integer>> res) {
+        int low = i + 1;
+        int high = nums.length - 1;
+        int sum = 0;
+        while (low < high) {
+            sum = nums[i] + nums[low] + nums[high];
+            if (sum < 0) {
+                low++;
+            } else if (sum > 0) {
+                high--;
+            } else {
+                List<Integer> ans = new ArrayList<>();
+                ans.add(nums[i]);
+                ans.add(nums[low++]);
+                ans.add(nums[high--]);
+                res.add(ans);
+                while (low < high && nums[low] == nums[low - 1]) {
+                    low++;
+                }
+            }
+        }
+    }
+}
