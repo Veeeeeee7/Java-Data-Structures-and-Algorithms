2e4ad3c5956fe65ae9d66db5fc234cb07f926823
victorli999 <victorli1076@gmail.com>
2023-05-23 20:47:01 -0700

    Move Zeroes

--- Algorithms/TwoPointer.java
+++ Algorithms/TwoPointer.java
@@ -44,4 +44,19 @@ public class TwoPointer {
             }
         }
     }
+
+    // Move Zeroes
+    public void moveZeroes(int[] nums) {
+        int pos = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[pos] = nums[i];
+                pos++;
+            }
+        }
+
+        for (int i = pos; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
 }
