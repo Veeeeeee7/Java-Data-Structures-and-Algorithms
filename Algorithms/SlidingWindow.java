d63dde02dfc6702363c96e7b75db29c895165295
victorli999 <victorli1076@gmail.com>
2023-06-07 15:55:17 -0700

    Max Number of Vowels

--- Algorithms/SlidingWindow.java
+++ Algorithms/SlidingWindow.java
@@ -19,4 +19,32 @@ public class SlidingWindow {
 
         return (double) ans / k;
     }
+
+    // Maximum Number of Vowels in a Substring of Given Length
+    public int maxVowels(String s, int k) {
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
+            }
+        }
+        int max = count;
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
+            }
+            if (isVowel(s.charAt(i - k))) {
+                count--;
+            }
+
+            if (count > max) {
+                max = count;
+            }
+        }
+        return max;
+    }
+
+    public boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
 }
