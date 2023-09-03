bec93a1a3dacc3fe42eddfe38a67b022321c4594
Victor <victorli1076@gmail.com>
2023-09-03 13:44:15 -0700

    Finished Sliding Window

--- Algorithms/SlidingWindow.java
+++ Algorithms/SlidingWindow.java
@@ -47,4 +47,44 @@ public class SlidingWindow {
     public boolean isVowel(char c) {
         return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
     }
+
+    // Max Consecutive Ones III
+    public int longestOnes(int[] nums, int k) {
+        int left = 0;
+        int right = 0;
+        while (right < nums.length) {
+            if (nums[right] == 0) {
+                k--;
+            }
+            if (k < 0) {
+                k++;
+                k -= nums[left];
+                left++;
+            }
+            right++;
+        }
+        return right - left;
+    }
+
+    // longest Subarray of 1's After Deleting One Element
+    public int longestSubarray(int[] nums) {
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        int zero = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
+                while (zero > 1) {
+                    if (nums[left] == 0) {
+                        zero--;
+                    }
+                    left++;
+                }
+            }
+            right++;
+            max = Math.max(max, right - left);
+        }
+        return max - 1;
+    }
 }
